e2c359aa8bf90c9b2243adb1bd5bcafde031948b
“dam-san” <ryococokuronene@gmail.com>
2020-12-04 09:37:05 +0000

    “###”

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+   get 'home/about' => 'homes#about'
+  root 'homes#top'
+  
+ 
+  
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  
+
+end
